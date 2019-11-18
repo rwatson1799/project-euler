ee5cd39eaae1d863a017839e118664f30b0eaebd
Ryan Watson <rwatson1799@gmail.com>
2019-11-18 16:58:07 +1000

    Editted the code to practice using a generator expression.

--- problem2.py
+++ problem2.py
@@ -1,14 +1,15 @@
+import numpy as np
+
 fibonacci = [1, 2]
-even_fib = [2]
 
 val = sum(fibonacci)
 while val < 4e6:
     fibonacci.append(val)
-    if val % 2 == 0:
-        even_fib.append(val)
-
     val = sum(fibonacci[-2:]) #sum the last two terms
 
 print(fibonacci)
+
+even_fib = (x for x in fibonacci if x % 2 == 0)
 print(even_fib)
 print(sum(even_fib))
+
